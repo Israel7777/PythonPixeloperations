991d311c726ca4f08b8e96e3ee9cc6025a8ac397
Maria Israel Sathyan <52373838+Israel7777@users.noreply.github.com>
2021-02-26 17:50:26 +0530

    Create Count_Color_pixel_in32bit_image.py
    finding the number of pixel of specific color in a image

---
+++ Count_Color_pixel_in32bit_image.py
@@ -0,0 +1,49 @@
+#importing Python pillow library for image processing 
+from PIL import Image
+
+#method to find out black pixels 
+def to_countcolorPixel():
+  
+    #to open image , image to be placed in same directory as the code is placed 
+    img = Image.open('[image name].[image format]')# ex : testimage.png
+    
+    #list containing the RGB value of color pixel
+    count32bitlist = [<rgb value>] #RGB Value of any colour ex: black [0,0,0]
+    
+    #x = iteration variable  , countcolor = count variable for color pixel initialized to zero
+    x =countcolor = 0
+    
+    #while loop to loop the width of the image 
+    while x < img.width:
+        
+        #y = iteration variable  
+        y = 0
+        
+        #while loop to loop the height of the image 
+        while y < img.height:
+          
+            #value of coordinates taken
+            coordinate = x, y
+            
+             #list containing the RGB value of specific pixel (x,y)
+            list1 = list(img.getpixel(coordinate))
+            
+            #comparing if the pixel is black
+            if list1[0] == color32bitlist[0]:
+                if list1[1] == color32bitlist[1]:
+                  if list1[2] == color32bitlist[2]:
+                        countcolor = countcolor + 1
+                
+            y = y + 1
+        x = x + 1
+        
+    #print number of black pixels
+    print(countcolor)
+
+#define main method
+def main():
+    to_countcolorPixel()
+
+#main method definition 
+if __name__ == "__main__":
+    main()
